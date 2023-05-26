fac26c8c62d6591373a57aac0fba1bec40d08518
Jasmine Osborne <jasosbo@chbc.school.nz>
2023-05-26 13:22:12 +1200

    Verison 1 - farm animal -finshed trailing and testing user input version - start of menu choice version

---
+++ Farm_Feeding_App/FarmAnimal.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm_Feeding_App
+{
+    internal class FarmAnimal
+    {
+        //atributes
+        private string species;
+        private string breed;
+        private string name;
+        private DateTime dob;
+        private List<float> foodConsumed;
+
+
+        //methods/functions
+
+        //constructs a farm animal object
+        public FarmAnimal(string species, string breed,string name, DateTime dob)
+        {
+            this.species = species;
+            this.breed = breed;
+            this.name = name;   
+            this.dob = dob; 
+            this.foodConsumed = new List<float>();
+        }
+
+        // add daily consumption to consumption list
+        public void AddDailyConsumption(float dailyConsumption)
+        {
+            
+            foodConsumed.Add(dailyConsumption); 
+
+        }
+
+        //Calculate animals food consumption over the week
+        public float WeeklyFoodConsumption()
+        {
+            float sumConsumption = 0;
+
+            foreach (float consumption in foodConsumed)
+            {
+            
+                sumConsumption += consumption;
+            }
+
+            return sumConsumption;
+            
+            
+        }
+        float weight = 20f;
+        float cost = 33.05f;
+
+        //Calculate animals food cost over a week
+        public float WeeklyFoodCost(float weight, float cost)
+        {
+
+            //retrieve cost per kg
+            
+
+
+            //calculate cost per unit
+            float costPerUnit = cost/ weight;
+
+            //calculate weekly cost - multiply cost per gram by the sumConsumption
+            return (float)Math.Round(WeeklyFoodConsumption() * costPerUnit, 2);
+
+
+        }
+        public string ConsumptionBreakdown()
+        {
+            string consumptionBreakdown = "";
+            int dayCounter = 0;
+            foreach (float consumption in foodConsumed)
+            {
+
+                consumptionBreakdown += $"Day {dayCounter + 1}; {consumption}\n";
+
+            }
+            return consumptionBreakdown;
+        }
+
+       
+
+        public string AnimalSummary()
+        {
+
+
+            return "----- Aniaml Summary-----\n" +
+             $"Species:{species}\n" +
+             $"Breeds:{breed}\n" +
+             $"Name:{name}\n" +
+             $"Date of Birth:{dob.ToString("d", CultureInfo.GetCultureInfo("es-ES"))}\n" +
+             ConsumptionBreakdown() +
+             $"weekly Food Consumption: {WeeklyFoodConsumption()}g\n" +
+             "Weekyly Food Cost: $"+WeeklyFoodCost(weight,cost); 
+
+
+        }
+
+
+
+
+    }
+}
--- Farm_Feeding_App/Program.cs
+++ Farm_Feeding_App/Program.cs
@@ -1,2 +1,152 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Dynamic;
+
+namespace Farm_Feeding_App
+{
+    internal class Program
+    {
+        static Dictionary<string, string[]> animalsDictionary = new Dictionary<string, string[]>()
+        {
+            {"Chicken", new string[]{"Brown", "Silkie" } } ,
+            {"Duck", new string[]{"Pekin", "Silver Bantam" } } ,
+            {"Alpaca", new string[]{"Suri", "Huacaya" } } ,
+            {"Goat", new string[]{"Nubian", "Pygmy" } }
+        };
+        
+
+        public static void Main(string[] args)
+        {
+
+
+
+            //ask and store animal details
+            
+
+            Console.WriteLine(SpeciesMenu());
+            string species = FindSpecies(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine(BreedMenu(species));
+            string breed = Console.ReadLine();
+
+           
+            string name = CapitiliseName("Enter animal name");
+
+            //date of birth breakdown
+            Console.WriteLine("Enter animal year of birth");
+            int dobYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter animal month of birth");
+            int dobMonth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter animal day of birth");
+            int dobDay = Convert.ToInt32(Console.ReadLine());
+            DateTime dob = new DateTime(dobYear, dobMonth, dobDay);
+
+            //create a new animal object
+            FarmAnimal testAnimal = new FarmAnimal(species, breed, name, dob);
+
+
+            for (int day = 1; day < 8; day++)
+            {
+                Console.WriteLine($"Enter animal food consumption day {day}");
+                testAnimal.AddDailyConsumption(Convert.ToInt32(Console.ReadLine()));
+            }
+
+
+
+
+
+            //display animal summary
+
+
+            Console.WriteLine(testAnimal.AnimalSummary());
+
+
+
+        }
+
+        private static string SpeciesMenu()
+        {
+
+            string menu = "Choose your animal\n";
+            int animalCounter = 1;
+
+            foreach  (KeyValuePair<string, string[]> animalType in animalsDictionary)
+            {
+                menu += $"{animalCounter}. {animalType.Key}\n";
+                animalCounter++;
+            }
+            return menu;
+        }
+
+        private static string FindSpecies(int menuChoice)
+        {
+
+            int speciesConter = 0;
+            string foundSpeices = "";
+
+
+            foreach (KeyValuePair<string, string[]> animalType in animalsDictionary)
+            {
+                speciesConter ++;
+                if (menuChoice == speciesConter)
+                {
+                    foundSpeices = animalType.Key;
+                    break;
+                }
+                Console.WriteLine(speciesConter);
+            }
+            
+            return foundSpeices;
+        }
+
+        private static string[] GetBreed(string species)
+        {
+            return animalsDictionary[species];
+
+
+        }
+
+        private static string BreedMenu(string species)
+        {
+
+            string[] breeds = GetBreed(species);
+            string menu = "Choose Breed\n";
+
+            for (int breedIndex = 0; breedIndex < breeds.Length; breedIndex++)
+            {
+
+                menu += (breedIndex + 1) + ". " + breeds[breedIndex] + "\n";
+
+            }
+            return menu;
+        }
+
+
+        private static string CapitiliseName(string question)
+        {
+            while (true)
+            {
+                Console.WriteLine(question);
+
+                string name = Console.ReadLine();
+
+                if (name.Length == 0)
+                    System.Console.WriteLine("Error: Names cannot be blank");
+                else
+                {
+                    name = char.ToUpper(name[0]) + name.Substring(1);
+
+                    return name;
+                }
+                    
+            }
+            
+        }
+    }
+}
